d60d73727cf1f0dc67254d16d1824e39d36cec0f
Bahtiar `kalkin` Gadimov <bahtiar@gadimov.de>
2022-03-17 00:28:27 +0100

    dev: Use bkg_lsp.on_attach()

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -1,43 +1,12 @@
 local nvim_lsp = require('lspconfig')
-local saga = require 'lspsaga' -- UI wrapper around LSP
-
-local on_attach = function(client, bufnr)
-    --require'completion'.on_attach(client, bufnr)
-
-    saga.init_lsp_saga()
-
-    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-    --Enable completion triggered by <c-x><c-o>
-    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-    local opts = { noremap=true, silent=true }
-    buf_set_keymap('n', '<C-]>', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    buf_set_keymap('n', 'gr', "<cmd>:TroubleToggle lsp_references<CR>", opts)
-    buf_set_keymap('n', 'K', "<cmd>lua require('lspsaga.hover').render_hover_doc()<CR>", opts)
-    buf_set_keymap('n', '<C-q>', "<cmd>lua require('lspsaga.signaturehelp').signature_help()<CR>", opts)
-    buf_set_keymap('n', '<A-k>', "<cmd>lua require'lspsaga.provider'.preview_definition()<CR>", opts)
-
-    buf_set_keymap('n', '<C-b>', "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>", opts)
-    buf_set_keymap('n', '<C-f>', "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>", opts)
-    buf_set_keymap('n', '<PageUp>', "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>", opts)
-    buf_set_keymap('n', '<PageDown>', "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>", opts)
-
-    buf_set_keymap('n', '<leader>ca', "<cmd>lua require('lspsaga.codeaction').code_action()<CR>", opts)
-    buf_set_keymap('v', '<leader>ca', ":<C-U>lua require('lspsaga.codeaction').range_code_action()<CR>", opts)
-    buf_set_keymap('n', '<leader>r', "<cmd>lua require('lspsaga.rename').rename()<CR>", opts)
-
-end
+local bkg_lsp = require('bkg-lsp')
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
 local servers = { "flow", "jedi_language_server",  "rust_analyzer", "phpactor", "pylsp" }
 for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup {
-        on_attach = on_attach,
+        on_attach = bkg_lsp.on_attach,
         flags = {
             debounce_text_changes = 150,
         }
