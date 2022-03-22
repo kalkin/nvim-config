d90b4b2fb8525bb840a661b38eb1e8366ca112f0
Bahtiar `kalkin` Gadimov <bahtiar@gadimov.de>
2022-03-22 15:31:38 +0100

    improvement: Monkeypatch vim.diagnostic.open_float

--- plugin/diagnostics.lua
+++ plugin/diagnostics.lua
@@ -6,35 +6,9 @@
 -- is an extension to existing error handling functionality such as the
 -- |quickfix| list.
 
-local border = {
-      {"🭽", "FloatBorder"},
-      {"▔", "FloatBorder"},
-      {"🭾", "FloatBorder"},
-      {"▕", "FloatBorder"},
-      {"🭿", "FloatBorder"},
-      {"▁", "FloatBorder"},
-      {"🭼", "FloatBorder"},
-      {"▏", "FloatBorder"},
-}
-
-local signs = { Error = '⛔', Warn = ' ', Hint = '💡', Info = '📌' }
+local signs = { Error = "⛔", Warn = " ", Hint = "💡", Info = "📌" }
 for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
-
-vim.diagnostic.config({
-    virtual_text = {
-        prefix = '👉',
-        source  = "always",
-    },
-    float = {
-        border = border,
-        focusable = false,
-        header = { '' .. ' Diagnostics:', 'Normal' },
-        source = 'always',
-    },
-    signs = true,
-    update_in_insert = false,
-    underline = true,
-});
+vim.diagnostic.open_float = require("lspsaga.diagnostic").show_line_diagnostics
